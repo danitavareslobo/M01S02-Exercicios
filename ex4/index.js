e9ee310d87e6ccfd699bc7df9e553c22887c9c76
DaniTavaresLobo <danitavares.dev@gmail.com>
2022-12-12 21:58:21 -0300

    Somar números

---
+++ ex4/index.js
@@ -0,0 +1,13 @@
+var soma = 0
+
+do {
+  var numero = parseInt(prompt('Digite um número'));
+
+  if (isNaN(numero) || numero === -1) {
+    continue;
+  }
+
+  soma += numero;
+} while (numero !== -1);
+
+alert(soma);
